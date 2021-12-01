ce02d621226c7a934ae24a2e64fbb9e770812f13
gustavogonzalez12 <61518365+gustavogonzalez12@users.noreply.github.com>
2021-12-01 12:53:54 +0100

    Add files via upload

---
+++ interfaces_copy/tmpaddpat.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfaces;
+
+import pojos.Patient;
+
+/**
+ *
+ * @author gustavo
+ */
+public class tmpaddpat {
+    
+    public static Patient patient;
+    
+    public static Patient getPatient(){
+        return patient;
+    }
+    
+     public static void  setPatient(Patient apatient){
+        patient=apatient;
+    }
+}
